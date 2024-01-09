8b4f4e45a82e23563aad70dcb29e9e753301559d
HorroGlitch <lverot@edenschool.fr>
2024-01-09 16:32:50 +0100

    add scrollbar value

--- main.js
+++ main.js
@@ -4,26 +4,36 @@ let back = document.querySelector("#back");
 let skip = document.querySelector("#skip");
 let reset = document.querySelector("#reset");
 let video = document.querySelector("#myVideo");
+let scrollbar = document.querySelector("#scrollbar");
 
 back.addEventListener("click", ()=>{
-    console.log("Wanna get back?");
-    video.currentTime -= 10
-})
+    video.currentTime -= 10;
+});
 
 skip.addEventListener("click", ()=>{
-    console.log("Wanna skip?");
-    video.currentTime += 10
-})
+    video.currentTime += 10;
+});
 
 reset.addEventListener("click", ()=>{
-    console.log("Let's get back from the start");
     video.load();
-})
+});
 
 video.addEventListener("click", ()=>{
+    scrollbar.setAttribute('max', parseInt(video.duration));
     if (video.paused) {
         video.play();
     } else {
         video.pause();
     }
 });
+
+scrollbar.addEventListener("input", ()=>{
+    
+    video.currentTime = scrollbar.value
+})
+
+video.addEventListener("timeupdate", ()=>{
+    scrollbar.value = video.currentTime;
+    // scrollbarMax.value = video.currentTime;
+});
+
